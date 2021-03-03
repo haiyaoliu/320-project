d09157fb8d07f1eab284c2aa6073bde4a88cc7ab
robtac <robbie9513@gmail.com>
2021-03-02 20:41:10 -0500

    Added login page and a parent layout component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Login from "./components/Login/Login";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Login />, document.getElementById("root"));
