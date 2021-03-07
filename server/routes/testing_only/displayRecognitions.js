62ec55e9ab251ddc89e413d86128f82f9a6ea2e9
mollydcarroll <mollydcarroll@gmail.com>
2021-03-07 16:22:44 -0500

    Create API Endpoint for displayRecognitions

--- server/routes/testing_only/displayRecognitions.js
+++ server/routes/testing_only/displayRecognitions.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const router = require("express").Router();
-const faker = require('faker'); //fake data
+//const faker = require('faker'); //fake data
 
-const randomParagraph = faker.lorem.paragraphs(); //fake recognition paragraph
+//const randomParagraph = faker.lorem.paragraphs(); //fake recognition paragraph
 
 mongoose.connect(
   "mongodb+srv://longnguyen:CS320Project@cs320db.aiuni.mongodb.net/cs320?retryWrites=true&w=majority"
@@ -12,11 +12,11 @@ require("../../models/Write");
 const Recognitions = mongoose.model("Write");
 
 router.get("/", (req, res, done) => {
-  /*Recognitions.find({}, function(err, userPost) {
+  Recognitions.find({}, function(err, recognitions) {
     if (err) return console.error(err);
-      res.send(userPost); //send real data
-  });*/
-  res.send(randomParagraph); //send fake data
+      res.send(recognitions); //send real data
+  });
+  //res.send(randomParagraph); //send fake data
 });
 
 module.exports = router;
