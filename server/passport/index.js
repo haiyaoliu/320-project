7d8c06338eeccfcc76a94af86ba24a6f28c60dda
Long Nguyen <minhlong179020@gmail.com>
2021-03-07 23:57:50 -0500

    Fix Warning Problem

--- server/passport/index.js
+++ server/passport/index.js
@@ -5,7 +5,8 @@ const JWTstrategy = require("passport-jwt").Strategy;
 const ExtractJWT = require("passport-jwt").ExtractJwt;
 
 mongoose.connect(
-  "mongodb+srv://longnguyen:CS320Project@cs320db.aiuni.mongodb.net/cs320?retryWrites=true&w=majority"
+  "mongodb+srv://longnguyen:CS320Project@cs320db.aiuni.mongodb.net/cs320?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
 require("../models/Users");
--- server/routes/testing_only/displayUsers.js
+++ server/routes/testing_only/displayUsers.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const router = require("express").Router();
 
 mongoose.connect(
-  "mongodb+srv://longnguyen:CS320Project@cs320db.aiuni.mongodb.net/cs320?retryWrites=true&w=majority"
+  "mongodb+srv://longnguyen:CS320Project@cs320db.aiuni.mongodb.net/cs320?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
 require("../../models/Users");
