030fdca4be4cfef4865ef5ee83d8b27b07b9564e
mollydcarroll <mollydcarroll@gmail.com>
2021-03-14 21:31:47 -0400

    Create API endpoint for displayRecognitions

--- server/routes/feed/displayRecognitions.js
+++ server/routes/feed/displayRecognitions.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const router = require("express").Router();
 
 require("../../models/Write");
-const Recognitions = mongoose.model("Write");
+const Recognition = mongoose.model("Write");
 
 router.get("/", async (req, res, done) => {
-  await Recognitions.find({}, function(err, recognitions) {
+  await Recognition.find({}, function(err, recognitions) {
     if (err) return console.error(err);
     res.send(recognitions);
   });
