fe6c632404edaac60e180a6e129fa54cabe821a6
robtac <robbie9513@gmail.com>
2021-03-15 16:07:17 -0400

    Added layout to dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Layout from "../../Layout";
 
 function Dashboard(props) {
     return (
-      <div>
+      <Layout>
           <p>dashboard</p>
           <a href="/">logout</a>
-      </div>
+      </Layout>
     );
 }
 
