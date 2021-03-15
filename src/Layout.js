5be74eec8955e585a200044c6e4fa6d160b5fe98
robtac <robbie9513@gmail.com>
2021-03-15 15:42:53 -0400

    Added stand-in Navbar

--- src/Layout.js
+++ src/Layout.js
@@ -8,14 +8,14 @@ function Layout(props) {
 
     return (
         <div>
-            {/* <Navbar bg="dark" variant="dark">
+            <Navbar bg="dark" variant="dark">
                 <Navbar.Brand href="#home">UKG</Navbar.Brand>
                 <Nav className="mr-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
                     <Nav.Link href="#features">Features</Nav.Link>
                     <Nav.Link href="#pricing">Pricing</Nav.Link>
                 </Nav>
-            </Navbar> */}
+            </Navbar>
             {children}
         </div>
     )
