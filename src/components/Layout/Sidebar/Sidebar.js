2396ea8b7bb6fb5492322cabc417cd71f024b7d0
khouiz <78759878+khouiz@users.noreply.github.com>
2021-04-12 17:49:43 -0400

    persistent login for new sidebar

--- src/components/Layout/Sidebar/Sidebar.js
+++ src/components/Layout/Sidebar/Sidebar.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Nav } from "react-bootstrap";
+import { getUser, removeUserSession } from '../../../utils/Common';
 
 function Sidebar(props) {
+    const user = getUser();
+
+    // handle click event of logout button
+    const handleLogout = () => {
+        removeUserSession();
+    }
+
     return (
         <Nav className="col-md-12 d-none d-md-block sidebar">
             <Nav.Link href="#settings">Top Employees</Nav.Link>
             <Nav.Link>Filter</Nav.Link>
-            <Nav.Link href="/">Log Out</Nav.Link>
+            <Nav.Link href="/" onClick={handleLogout}>Log Out</Nav.Link>
             <href></href>
         </Nav>);
 }
