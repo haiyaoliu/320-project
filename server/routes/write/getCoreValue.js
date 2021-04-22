e16befbe5cf81adc651c185b1ac664bfb9b120f3
Thanh Phan <phanchithanh091@gmail.com>
2021-04-22 20:58:47 +0700

    filter by corevalue and myrecognition

--- server/routes/write/getCoreValue.js
+++ server/routes/write/getCoreValue.js
@@ -5,10 +5,14 @@ require("../../models/CoreValues");
 const CoreValues = mongoose.model("CoreValues");
 
 router.post("/", (req, res, done) => {
-    CoreValues.findOne({ companyName: req.fields.company }, "values", function (err, coreValue) {
-        if (err) return console.error(err);
-        res.send(coreValue);
-    });
+  CoreValues.findOne(
+    { companyName: req.fields.company },
+    "values",
+    function (err, coreValue) {
+      if (err) return console.error(err);
+      res.send(coreValue);
+    }
+  );
 });
 
 module.exports = router;
