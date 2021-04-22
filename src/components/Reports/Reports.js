a005e29f12787922fa19a9d467d6d0dc93fefc42
Long Nguyen <minhlong179020@gmail.com>
2021-04-22 13:44:42 -0400

    update reported page header

--- src/components/Reports/Reports.js
+++ src/components/Reports/Reports.js
@@ -9,30 +9,54 @@ import axios from "axios";
 
 
 function Reports(props) {
+    const [userInfo, setUserInfo] = useState({})
+    
+    useEffect(() => {
+        let email = localStorage.getItem('user')
+        let emailString = email.slice(1, email.length - 1)
 
+        axios.post("write/getCurrentUser", { email: emailString }).then((response) => {    
+            let info = {
+                email: emailString,
+                fullName: response.data["firstName"] + ' ' + response.data["lastName"],
+                company: response.data["companyName"],
+                position: response.data["positionTitle"]
+            };
+            setUserInfo(info);
+            
+        }).catch(error => {
+            console.log('There was an error!', error);
+        })
+
+    
+    }, [])
+    
+    
+    
     return (
         <Layout>
             <Container fluid>
                 <div className="profile-header">
                     <div className="profile-header-cover">
-                        {/* #TODO HERE */}
+                        <p>Access: <span style={{ "color": "red"}}>Manager</span></p>
                     </div>
                     <div className="profile-header-content">
                         <div className="profile-header-avatar">
                             <Image src="Avatar.png" roundedCircle></Image>
+                            <p>{userInfo.fullName}</p>
                         </div>
                         <div className="profile-header-info">
                             <p className="profile-position">
-                                Senior Software Engineer at GreenLife Consulting
+                                {userInfo.position} at {userInfo.company}
                             </p>
                             <p className="profile-contact">
-                                Contact: Arron_Garcia@greenlifeconsulting.com
+                                Contact: {userInfo.email}
                             </p>
                         </div>
                     </div>
                 </div>
                 <div className="profile-content">
-                    <Posts></Posts> {/*Replace this later */}
+                    <Posts></Posts> 
                 </div>
             </Container>
         </Layout>
