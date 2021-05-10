1d864df26921a1012b32cc5e0eb2b6950faed1cd
Norapat Rerngkasetkig <78759890+nrerngkasetk@users.noreply.github.com>
2021-05-10 15:53:11 -0400

    Removed weird gray line.

--- src/components/MyData/Tally/Tally.js
+++ src/components/MyData/Tally/Tally.js
@@ -42,7 +42,7 @@ export const TallyList = (props) => {
                         let f = 2.0 - 0.15*len;
                         let fsize = (f > 0.5 ? f : 0.5);
                         return(
-                            <Col className="justify-content-md-center" md={{span:Math.min(8/len, 5)}}>
+                            <Col className="justify-content-md-center" md={{span:Math.min(8/len, 5)}} style={{borderRight:`0px`}}>
                                 <CircularProgressbarWithChildren 
                                     className="tally-counter"
                                     value={recognitionData[key] % 100}
